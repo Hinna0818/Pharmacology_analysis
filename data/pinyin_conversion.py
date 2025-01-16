4d90689798b2431e2611f399b72701353cdafd5b
Hinna He <hinna@i.smu.edu.cn>
2025-01-16 20:18:55 +0800

    Update pinyin_conversion.py

--- data/pinyin_conversion.py
+++ data/pinyin_conversion.py
@@ -3,8 +3,8 @@ from pypinyin import lazy_pinyin
 #print(''.join(lazy_pinyin('地龙')))
 
 ## 读取数据
-data = pyreadr.read_r('./herb_data.rda')
-result = data['herb_data']
+data = pyreadr.read_r('./input your herb data.rda')
+result = data['your herb data name']
 
 # 将非字符串类型的数据转换为字符串
 result['Herb_cn_name'] = result['Herb_cn_name'].astype(str)
@@ -12,6 +12,6 @@ result['Herb_cn_name'] = result['Herb_cn_name'].astype(str)
 # 将中文名转换为拼音名
 result['Herb_pinyin_name'] = result['Herb_cn_name'].apply(lambda x: ''.join(lazy_pinyin(x)))
 
-pyreadr.write_rdata('./herb_data.rda', result)
+pyreadr.write_rdata('./ouput_data.rda', result)
 
 
